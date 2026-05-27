6deb25ebbfda3e55dc1ddcde56b3ed9a3b20ce89
shoemrahman-sys <shoemrahman@gmail.com>
2026-05-27 13:42:24 +0530

    Ready for deployment

--- config/database.py
+++ config/database.py
@@ -7,6 +7,7 @@ load_dotenv()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
